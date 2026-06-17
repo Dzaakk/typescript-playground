599b8b9dc97a3bca6e9bdbfbf40092fdcd0290a6
Dzkzzz <dzdzaakk@gmail.com>
2026-06-18 05:52:14 +0700

    nested object types

--- beginner_course/type_basics/type.ts
+++ beginner_course/type_basics/type.ts
@@ -6,19 +6,38 @@ let isStudent: boolean = false
 type Food = string
 let favoriteFood: Food = "pizza"
 
+type Address = {
+    street: string
+    city: string
+    country: string
+}
+
 type Person = {
     name: string
     age: number
     isStudent: boolean
+    address: Address
+
 }
+
 let person: Person = {
     name: "Budi",
     age: 26,
-    isStudent: false
+    isStudent: false,
+    address: {
+        street: "12 Main",
+        city: "Anytown",
+        country: "USA"
+    }
 }
 
 let person2: Person = {
     name: "gol",
     age: 14,
-    isStudent: true
+    isStudent: true,
+    address: {
+        street: "13 Main",
+        city: "Anytown",
+        country: "USA"
+    }
 }
