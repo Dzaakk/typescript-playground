d6548167af6f44f36f5b090b2192456863be026b
Dzkzzz <dzdzaakk@gmail.com>
2026-06-18 05:41:34 +0700

    add custom types

--- beginner_course/type_basics/type.ts
+++ beginner_course/type_basics/type.ts
@@ -2,3 +2,23 @@
 let myName: string = "Dz"
 let numberOfWheels: number = 4
 let isStudent: boolean = false
+
+type Food = string
+let favoriteFood: Food = "pizza"
+
+type Person = {
+    name: string
+    age: number
+    isStudent: boolean
+}
+let person: Person = {
+    name: "Budi",
+    age: 26,
+    isStudent: false
+}
+
+let person2: Person = {
+    name: "gol",
+    age: 14,
+    isStudent: true
+}
